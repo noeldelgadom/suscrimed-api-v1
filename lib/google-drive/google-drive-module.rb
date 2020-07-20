bc2350030f9c753f8d397d930d16a42a9b491303
Noel Delgado <noeldelgadom@gmail.com>
2020-07-19 19:17:18 -0500

    added new key and gintignnored it

--- lib/google-drive/google-drive-module.rb
+++ lib/google-drive/google-drive-module.rb
@@ -3,7 +3,7 @@ module GoogleDriveModule
   Bundler.require
 
   def first_try
-    session     = GoogleDrive::Session.from_service_account_key("config/api-keys/google-sheets-keys.json")
+    session     = GoogleDrive::Session.from_service_account_key("config/api-keys/google-sheets-key.json")
     spreadsheet = session.spreadsheet_by_title("CATALAGO DE PRODUCTOS NADRO AL 200712")
     worksheet   = spreadsheet.worksheets.first
     worksheet.rows.each { |row| puts row.first(6).join(" | ") }
