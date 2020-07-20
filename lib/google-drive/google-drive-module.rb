f48cc2a85cd88866b4403996332340327a3a0913
Noel Delgado <noeldelgadom@gmail.com>
2020-07-19 19:55:30 -0500

    Write 1, 2 and 3 on test Sheet

--- lib/google-drive/google-drive-module.rb
+++ lib/google-drive/google-drive-module.rb
@@ -2,10 +2,16 @@ module GoogleDriveModule
   require 'bundler'
   Bundler.require
 
+  # reload! ; load "lib/google-drive/google-drive-module.rb" ; include GoogleDriveModule ; first_try
+
   def first_try
     session     = GoogleDrive::Session.from_service_account_key("config/api-keys/google-sheets-key.json")
-    spreadsheet = session.spreadsheet_by_title("CATALAGO DE PRODUCTOS NADRO AL 200712")
+    spreadsheet = session.spreadsheet_by_title("Practice API")
     worksheet   = spreadsheet.worksheets.first
-    worksheet.rows.each { |row| puts row.first(6).join(" | ") }
+    worksheet.rows.first(3).each_with_index do |row, i|
+      worksheet[i + 1,2] = i + 1
+    end
+
+    worksheet.save
   end
 end
