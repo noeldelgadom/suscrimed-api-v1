a897badb3f30bd61859b15f5014502db6c6c2891
Noel Delgado <noeldelgadom@gmail.com>
2020-08-02 11:25:21 -0500

    set ID search

--- lib/web-scrapers/price_scraper_module.rb
+++ lib/web-scrapers/price_scraper_module.rb
@@ -11,6 +11,8 @@ module PriceScraperModule
   def scrape_ahorro(browser)
     puts 'Scrapeando Ahorro'
     browser.goto 'https://www.fahorro.com/'
+    browser.text_field(id: 'search').set "12345"
     byebug
+    browser.close
   end
 end
