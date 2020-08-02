81be6387965b179bd4d3e8e71ad313ef6c2d96c3
Noel Delgado <noeldelgadom@gmail.com>
2020-08-02 16:39:22 -0500

    fresko and guadalajara scraper + ahorro no stock

--- lib/web-scrapers/price_scraper_module.rb
+++ lib/web-scrapers/price_scraper_module.rb
@@ -4,13 +4,17 @@ module PriceScraperModule
   
   def self.scrape
     puts 'Starting Scrape'
-    ean     = "7501109901890"
+    # ean     = "1234567890123" # Fake EAN
+    # ean     = "7501109901890" # Pariet
+    ean     = "7501314704644" # Durater
     browser = Watir::Browser.new
 
     prices  = {}
     prices[:ahorro]       = scrape_ahorro(browser, ean)
-    prices[:city_market]  = scrape_city_market(browser, ean)
-    prices[:farmalisto]   = scrape_farmalisto(browser, ean)
+    # prices[:city_market]  = scrape_city_market(browser, ean)
+    # prices[:farmalisto]   = scrape_farmalisto(browser, ean)
+    # prices[:fresko]       = scrape_fresko(browser, ean)
+    # prices[:guadalajara]  = scrape_guadalajara(browser, ean)
 
     byebug
     prices = {:ahorro=>111.0, :city_market=>222.0, :farmalisto=>333.0}
@@ -20,7 +24,7 @@ module PriceScraperModule
   def scrape_ahorro(browser, ean)
     puts 'Scrapeando Ahorro. EAN: ' + ean
     browser.goto 'https://www.fahorro.com/catalogsearch/result/?q=' + ean
-    price = assign_price(browser.span(class: 'price'))
+    price = assign_price(browser.span(class: 'price'), browser.p(class: 'note-msg'))
   end
 
   def scrape_city_market(browser, ean)
@@ -35,8 +39,28 @@ module PriceScraperModule
     price = assign_price(browser.span(class: 'df-card__price'))
   end
 
-  def assign_price(element)
-    element.wait_until(&:exists?)
-    element.exists? ? element.text.gsub(/[^\d]/, '').to_f / 100 : 9876543210
+  def scrape_fresko(browser, ean)
+    puts 'Scrapeando Fresko. EAN: ' + ean
+    browser.goto 'https://www.lacomer.com.mx/lacomer/goBusqueda.action?succId=137&ver=mislistas&succFmt=100&criterio=' + ean + '#/' + ean
+    price = assign_price(browser.span(class: 'precio_normal'))
+  end
+
+  def scrape_guadalajara(browser, ean)
+    puts 'Scrapeando Fresko. EAN: ' + ean
+    browser.goto 'https://www.farmaciasguadalajara.com/SearchDisplay?storeId=10151&searchTerm=' + ean
+    price = assign_price(browser.span(class: 'price'))
+  end
+
+  def assign_price(success_element, failure_element)
+
+    # success_element.wait_until(&:exists?)
+
+    if success_element.exists?
+      success_element.text.gsub(/[^\d]/, '').to_f / 100
+    elsif failure_element.exists?
+      'Not In Store'
+    else
+      'Other Error'
+    end
   end
 end
