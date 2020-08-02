7e1f4bf751704f159fea79cfd0d3500a81d0b9d6
Noel Delgado <noeldelgadom@gmail.com>
2020-08-02 15:08:42 -0500

    added a wait_until on watir to elements

--- lib/web-scrapers/price_scraper_module.rb
+++ lib/web-scrapers/price_scraper_module.rb
@@ -8,33 +8,35 @@ module PriceScraperModule
     browser = Watir::Browser.new
 
     prices  = {}
-    # prices[:ahorro]       = scrape_ahorro(browser, ean)
-    # prices[:city_market]  = scrape_city_market(browser, ean)
-    # prices[:farmalisto]   = scrape_farmalisto(browser, ean)
+    prices[:ahorro]       = scrape_ahorro(browser, ean)
+    prices[:city_market]  = scrape_city_market(browser, ean)
+    prices[:farmalisto]   = scrape_farmalisto(browser, ean)
 
-    prices = {:ahorro=>1114.0, :city_market=>1030.0, :farmalisto=>1025.0}
+    byebug
+    prices = {:ahorro=>111.0, :city_market=>222.0, :farmalisto=>333.0}
     browser.close
   end
 
   def scrape_ahorro(browser, ean)
     puts 'Scrapeando Ahorro. EAN: ' + ean
     browser.goto 'https://www.fahorro.com/catalogsearch/result/?q=' + ean
-    price = clean_price(browser.span(class: 'price').text)
+    price = assign_price(browser.span(class: 'price'))
   end
 
   def scrape_city_market(browser, ean)
     puts 'Scrapeando City Market. EAN: ' + ean
     browser.goto 'https://www.lacomer.com.mx/lacomer/goBusqueda.action?succId=380&ver=mislistas&succFmt=200&criterio=' + ean + '#/' + ean
-    price = clean_price(browser.span(class: 'precio_normal').text)
+    price = assign_price(browser.span(class: 'precio_normal'))
   end
 
   def scrape_farmalisto(browser,ean)
     puts 'Scrapeando Farmalisto. EAN: ' + ean
     browser.goto 'https://www.farmalisto.com.mx/#/dffullscreen/query=' + ean + '&query_name=match_and'
-    price = clean_price(browser.span(class: 'df-card__price').text)
+    price = assign_price(browser.span(class: 'df-card__price'))
   end
 
-  def clean_price(text)
-    text.gsub(/[^\d]/, '').to_f / 100
+  def assign_price(element)
+    element.wait_until(&:exists?)
+    element.exists? ? element.text.gsub(/[^\d]/, '').to_f / 100 : 9876543210
   end
 end
