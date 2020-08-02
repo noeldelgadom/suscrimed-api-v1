2ef8b09f81d1d5484bbd80df3696dade2ac88035
Noel Delgado <noeldelgadom@gmail.com>
2020-08-02 11:06:20 -0500

    Watir working

--- lib/web-scrapers/price_scraper_module.rb
+++ lib/web-scrapers/price_scraper_module.rb
@@ -1,8 +1,9 @@
 module PriceScraperModule
-
-  # reload! ; load "lib/web-scrapers/price_scraper_module.rb" ; include PriceScraperModule ; scrape
   
-  def scrape
-    puts 'yo'
+  # reload! ; load "lib/web-scrapers/price_scraper_module.rb" ; include PriceScraperModule ; PriceScraperModule.scrape
+  
+  def self.scrape
+    puts 'yoooaa'
+    browser = Watir::Browser.new
   end
 end
