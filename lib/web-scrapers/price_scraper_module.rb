de7f4b8b2e0934a05e0db64a90322c090c5433dd
Noel Delgado <noeldelgadom@gmail.com>
2020-08-02 11:19:42 -0500

    initialized ahorro scrape

--- lib/web-scrapers/price_scraper_module.rb
+++ lib/web-scrapers/price_scraper_module.rb
@@ -3,7 +3,14 @@ module PriceScraperModule
   # reload! ; load "lib/web-scrapers/price_scraper_module.rb" ; include PriceScraperModule ; PriceScraperModule.scrape
   
   def self.scrape
-    puts 'yoooaa'
+    puts 'Starting Scrape'
     browser = Watir::Browser.new
+    scrape_ahorro(browser)
+  end
+
+  def scrape_ahorro(browser)
+    puts 'Scrapeando Ahorro'
+    browser.goto 'https://www.fahorro.com/'
+    byebug
   end
 end
