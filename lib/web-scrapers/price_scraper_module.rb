9b1a7592a9170a00b1e9758e7642d3fa62f9339a
Noel Delgado <noeldelgadom@gmail.com>
2020-08-02 12:52:59 -0500

    got price from f Ahorro

--- lib/web-scrapers/price_scraper_module.rb
+++ lib/web-scrapers/price_scraper_module.rb
@@ -7,6 +7,7 @@ module PriceScraperModule
     ean     = "7501109901890"
     browser = Watir::Browser.new
     scrape_ahorro(browser, ean)
+    browser.close
   end
 
   def scrape_ahorro(browser, ean)
@@ -17,11 +18,10 @@ module PriceScraperModule
     browser.li(class: 'item').click
 
     if ean = browser.table(id: 'product-attribute-specs-table').tr(class: 'even').td.text
-      puts 'Success! EAN matches'
-      byebug
+      price = browser.span(class: 'price').text.gsub(/[^\d]/, '').to_f / 100
     else
-      puts 'Error! EAN on the page is NOT the one we are looking for'
+      price = 9876543210        # If this shows, up it means the EAN on the page is NOT the one we are looking for
     end
-    browser.close
+    return price
   end
 end
