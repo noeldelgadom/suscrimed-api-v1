b51ceaf651a894938f0f075f3665fe0e5756a1dc
Noel Delgado <noeldelgadom@gmail.com>
2020-08-16 17:59:48 -0500

    succesful while loop that saves

--- lib/google-drive/google-drive-module.rb
+++ lib/google-drive/google-drive-module.rb
@@ -27,13 +27,36 @@ module GoogleDriveModule
     puts'--------------------------------------------------------------------------------'
     puts Time.now.to_s + ' Start Updating Prices'
 
-    row     = 2
-    ean     = worksheet[row, 1] # Pariet
-    puts Time.now.to_s + ' Updating EAN: ' + ean
-    prices  = PriceScraperModule.scrape_ean(ean)
+    row               = 2
+    save_every_x_rows = 20 
 
+    while worksheet[row, 1] != ''
+      ean     = worksheet[row, 1]
+      puts Time.now.to_s + ' Updating EAN: ' + ean
+      
+      prices  = PriceScraperModule.scrape_ean(ean)
 
-    byebug
+      worksheet[row, 3]   = prices[:ahorro]
+      worksheet[row, 4]   = prices[:by_price]
+      worksheet[row, 5]   = prices[:city_market]
+      worksheet[row, 6]   = prices[:farmalisto]
+      worksheet[row, 7]   = prices[:fresko]
+      worksheet[row, 8]   = prices[:guadalajara]
+      worksheet[row, 9]   = prices[:la_comer]
+      worksheet[row, 10]  = prices[:prixz]
+      worksheet[row, 11]  = prices[:san_pablo]
+      worksheet[row, 12]  = prices[:soriana]
+      worksheet[row, 13]  = prices[:chedraui]
+      worksheet[row, 14]  = prices[:superama]
+      worksheet[row, 15]  = prices[:walmart]
+      worksheet[row, 16]  = prices[:sanborns]  
+      
+
+      worksheet.save if row % save_every_x_rows == 0
+
+      row += 1
+    end
+
     puts Time.now.to_s + ' Finish Updating Prices'
     puts'--------------------------------------------------------------------------------'
     puts'--------------------------------------------------------------------------------'
