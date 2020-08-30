7da5a56d2fd0066e6dd9e49027521da3431f1370
Noel Delgado <noeldelgadom@gmail.com>
2020-08-30 15:56:51 -0500

    renamed today_prices

--- lib/google-drive/google-drive-module.rb
+++ lib/google-drive/google-drive-module.rb
@@ -7,22 +7,22 @@ module GoogleDriveModule
   # reload! ; load "lib/google-drive/google-drive-module.rb" ; include GoogleDriveModule ; GoogleDriveModule.update_competitor_prices
 
   def self.update_competitor_prices
-    session           = GoogleDrive::Session.from_service_account_key("config/api-keys/google-sheets-key.json")
-    spreadsheet       = session.spreadsheet_by_title("Competitor Prices")
-    worksheet_today   = spreadsheet.worksheets.first
+    session                 = GoogleDrive::Session.from_service_account_key("config/api-keys/google-sheets-key.json")
+    spreadsheet             = session.spreadsheet_by_title("Competitor Prices")
+    worksheet_today_prices  = spreadsheet.worksheets.first
 
     puts'--------------------------------------------------------------------------------'
     puts'--------------------------------------------------------------------------------'
     puts Time.now.to_s + ' Start Updating Prices'
 
     row = 2
-    while worksheet_today[row, 1] != ''
-      ean     = worksheet_today[row, 1]
+    while worksheet_today_prices[row, 1] != ''
+      ean     = worksheet_today_prices[row, 1]
       puts Time.now.to_s + ' Updating EAN: ' + ean
       
       prices  = PriceScraperModule.scrape_ean(ean)
 
-      GoogleDriveModule.update_today(row, prices, worksheet_today)
+      GoogleDriveModule.update_today(row, prices, worksheet_today_prices)
 
       row += 1
     end
