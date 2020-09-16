2ca836a0d848f7e5d5fa9be9f1bf7171b4cff51c
Noel Delgado <noeldelgadom@gmail.com>
2020-09-16 10:53:00 -0500

    added san_pablo image scrape

--- lib/web-scrapers/image_scraper_module.rb
+++ lib/web-scrapers/image_scraper_module.rb
@@ -1,14 +1,13 @@
 module ImageScraperModule
   
-  # reload! ; load "lib/web-scrapers/price_scraper_module.rb" ; include PriceScraperModule ; PriceScraperModule.scrape_ean("1234567890123")      # Fake EAN
-  
   def self.scrape_ean(browser, ean)
     puts'------------------'
     puts Time.now.to_s + ' Starting Scrape for EAN: ' + ean
 
-    image_url = ImageScraperModule.scrape_ahorro(browser, ean)
-    image_url = ImageScraperModule.scrape_farmalisto(browser, ean)  if image_url == ''
-    image_url = 'Not Found'                                         if image_url == ''
+    image_url = ImageScraperModule.scrape_ahorro(     browser, ean)
+    image_url = ImageScraperModule.scrape_san_pablo(  browser, ean)   if image_url == ''
+    image_url = ImageScraperModule.scrape_farmalisto( browser, ean)   if image_url == ''
+    image_url = 'Not Found'                                           if image_url == ''
     
     puts Time.now.to_s + ' Ending Scrape for EAN: ' + ean 
     puts'------------------'
@@ -20,6 +19,17 @@ module ImageScraperModule
     puts Time.now.to_s + ' Scrapeando Ahorro'
     browser.goto 'https://www.fahorro.com/catalogsearch/result/?q=' + ean
     browser.img(class: 'product-image-photo').exists? ? browser.img(class: 'product-image-photo').src : ''
+  end
+
+  def self.scrape_san_pablo(browser, ean)
+    puts Time.now.to_s + ' Scrapeando San Pablo'
+    browser.goto 'https://www.farmaciasanpablo.com.mx/search/?text=' + ean
+    if browser.img(class: 'item-image').exists?
+      nil while 'https' != browser.img(class: 'item-image').src[0..4]
+      browser.img(class: 'item-image').src
+    else
+      ''  
+    end
   end
 
   def self.scrape_farmalisto(browser,ean)
