985944424fd3fae8d07ce1d3cb6d4e5b217a44df
Noel Delgado <noeldelgadom@gmail.com>
2020-09-20 15:37:56 -0500

    hid the api_token succesfully

--- lib/webflow/webflow_module.rb
+++ lib/webflow/webflow_module.rb
@@ -5,9 +5,11 @@ module WebflowTool
   # reload! ; load "lib/webflow/webflow_module.rb" ; include WebflowTool ; WebflowTool.get_info
 
   def self.get_info
-    url       = 'https://api.webflow.com/info'
-    response  = RestClient.get url, {
-      'Authorization' => 'Bearer Actual-Token',
+    url           = 'https://api.webflow.com/info'
+    webflow_auth  = JSON.parse(File.read('config/api-keys/webflow-auth.json'))
+
+    response        = RestClient.get url, {
+      'Authorization' => 'Bearer ' + webflow_auth['api_token'],
       'accept-version' => '1.0.0'
     }
     JSON.parse(response.body)
