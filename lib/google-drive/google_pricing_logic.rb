0c6166e09cd8c86966bb84fc4697c199a97f023b
Noel Delgado <noeldelgadom@gmail.com>
2020-09-27 14:59:38 -0500

    calculated margin

--- lib/google-drive/google_pricing_logic.rb
+++ lib/google-drive/google_pricing_logic.rb
@@ -5,13 +5,13 @@ module GooglePricingLogic
   # reload! ; load "lib/google-drive/google_pricing_logic.rb" ; include GooglePricingLogic ; GooglePricingLogic.version_1
 
   def self.version_1
-    session                       = GoogleDrive::Session.from_service_account_key("config/api-keys/google-sheets-key.json")
+    # session                       = GoogleDrive::Session.from_service_account_key("config/api-keys/google-sheets-key.json")
     # competitor_prices_spreadsheet = session.spreadsheet_by_title("Competitor Prices")
     # competitor_prices             = competitor_prices_spreadsheet.worksheets.fifth
     # nadro_spreadsheet             = session.spreadsheet_by_title("NADRO - CATALAGO DE PRODUCTOS NADRO AL 200712")
     # nadro_costs                   = nadro_spreadsheet.worksheets.first
-    webflow_spreadsheet           = session.spreadsheet_by_title("Webflow Product Import")
-    webflow_import                = webflow_spreadsheet.worksheets.first
+    # webflow_spreadsheet           = session.spreadsheet_by_title("Webflow Product Import")
+    # webflow_import                = webflow_spreadsheet.worksheets.first
 
     puts'--------------------------------------------------------------------------------'
     puts'--------------------------------------------------------------------------------'
@@ -19,7 +19,17 @@ module GooglePricingLogic
 
     # puts competitor_prices[2,3]
     # puts nadro_costs[7,3]
-    puts webflow_import[1,1]
+    # puts webflow_import[1,1]
+
+    farmalisto_price  = 12.0
+    price_max_limit   = 15.0
+    cost              = 10.0
+    
+    price             = farmalisto_price
+    margin            = (price - cost) / cost
+
+    puts margin
+
 
     puts Time.now.to_s + ' Finish Updating Google Prices'
     puts'--------------------------------------------------------------------------------'
