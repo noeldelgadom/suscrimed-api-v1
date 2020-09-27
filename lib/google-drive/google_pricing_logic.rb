a0493a7ac1b62d430444de1d79113b1b48175878
Noel Delgado <noeldelgadom@gmail.com>
2020-09-27 13:33:05 -0500

    reading competitor prices

--- lib/google-drive/google_pricing_logic.rb
+++ lib/google-drive/google_pricing_logic.rb
@@ -5,13 +5,15 @@ module GooglePricingLogic
   # reload! ; load "lib/google-drive/google_pricing_logic.rb" ; include GooglePricingLogic ; GooglePricingLogic.version_1
 
   def self.version_1
-    # session                 = GoogleDrive::Session.from_service_account_key("config/api-keys/google-sheets-key.json")
-    # competitor_prices       = session.spreadsheet_by_title("Competitor Prices")
-    # worksheet_images        = spreadsheet.worksheets.first
+    session                       = GoogleDrive::Session.from_service_account_key("config/api-keys/google-sheets-key.json")
+    competitor_prices_spreadsheet = session.spreadsheet_by_title("Competitor Prices")
+    competitor_prices             = competitor_prices_spreadsheet.worksheets.fifth
 
     puts'--------------------------------------------------------------------------------'
     puts'--------------------------------------------------------------------------------'
     puts Time.now.to_s + ' Start Updating Google Prices'
+
+    puts competitor_prices[2,3]
 
     puts Time.now.to_s + ' Finish Updating Google Prices'
     puts'--------------------------------------------------------------------------------'
