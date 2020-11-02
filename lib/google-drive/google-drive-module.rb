894cfd93e029c278b196f36e332233c842b5cb6f
Noel Delgado <noeldelgadom@gmail.com>
2020-11-02 15:33:12 -0600

    Only updates prices > 0

--- lib/google-drive/google-drive-module.rb
+++ lib/google-drive/google-drive-module.rb
@@ -17,7 +17,7 @@ module GoogleDriveModule
     puts'--------------------------------------------------------------------------------'
     puts Time.now.to_s + ' Start Updating Prices'
 
-    row = 11001
+    row = 20001
     while worksheet_today_prices[row, 1] != '' && Time.now.hour < 21
       ean     = worksheet_today_prices[row, 1]
       puts Time.now.to_s + ' Updating EAN: ' + ean
@@ -56,20 +56,20 @@ module GoogleDriveModule
   end
 
   def self.update_last_prices(row, prices, worksheet)
-    worksheet[row, 3]   = prices[:ahorro]       if 'Float' == prices[:ahorro].class.name
-    worksheet[row, 4]   = prices[:by_price]     if 'Float' == prices[:by_price].class.name
-    worksheet[row, 5]   = prices[:city_market]  if 'Float' == prices[:city_market].class.name
-    worksheet[row, 6]   = prices[:farmalisto]   if 'Float' == prices[:farmalisto].class.name
-    worksheet[row, 7]   = prices[:fresko]       if 'Float' == prices[:fresko].class.name
-    worksheet[row, 8]   = prices[:guadalajara]  if 'Float' == prices[:guadalajara].class.name
-    worksheet[row, 9]   = prices[:la_comer]     if 'Float' == prices[:la_comer].class.name
-    worksheet[row, 10]  = prices[:prixz]        if 'Float' == prices[:prixz].class.name
-    worksheet[row, 11]  = prices[:san_pablo]    if 'Float' == prices[:san_pablo].class.name
-    worksheet[row, 12]  = prices[:soriana]      if 'Float' == prices[:soriana].class.name
-    worksheet[row, 13]  = prices[:chedraui]     if 'Float' == prices[:chedraui].class.name
-    worksheet[row, 14]  = prices[:superama]     if 'Float' == prices[:superama].class.name
-    worksheet[row, 15]  = prices[:walmart]      if 'Float' == prices[:walmart].class.name
-    worksheet[row, 16]  = prices[:sanborns]     if 'Float' == prices[:sanborns].class.name
+    worksheet[row, 3]   = prices[:ahorro]       if 'Float' == prices[:ahorro].class.name && 0 < prices[:ahorro]
+    worksheet[row, 4]   = prices[:by_price]     if 'Float' == prices[:by_price].class.name && 0 < prices[:by_price]
+    worksheet[row, 5]   = prices[:city_market]  if 'Float' == prices[:city_market].class.name && 0 < prices[:city_market]
+    worksheet[row, 6]   = prices[:farmalisto]   if 'Float' == prices[:farmalisto].class.name && 0 < prices[:farmalisto]
+    worksheet[row, 7]   = prices[:fresko]       if 'Float' == prices[:fresko].class.name && 0 < prices[:fresko]
+    worksheet[row, 8]   = prices[:guadalajara]  if 'Float' == prices[:guadalajara].class.name && 0 < prices[:guadalajara]
+    worksheet[row, 9]   = prices[:la_comer]     if 'Float' == prices[:la_comer].class.name && 0 < prices[:la_comer]
+    worksheet[row, 10]  = prices[:prixz]        if 'Float' == prices[:prixz].class.name && 0 < prices[:prixz]
+    worksheet[row, 11]  = prices[:san_pablo]    if 'Float' == prices[:san_pablo].class.name && 0 < prices[:san_pablo]
+    worksheet[row, 12]  = prices[:soriana]      if 'Float' == prices[:soriana].class.name && 0 < prices[:soriana]
+    worksheet[row, 13]  = prices[:chedraui]     if 'Float' == prices[:chedraui].class.name && 0 < prices[:chedraui]
+    worksheet[row, 14]  = prices[:superama]     if 'Float' == prices[:superama].class.name && 0 < prices[:superama]
+    worksheet[row, 15]  = prices[:walmart]      if 'Float' == prices[:walmart].class.name && 0 < prices[:walmart]
+    worksheet[row, 16]  = prices[:sanborns]     if 'Float' == prices[:sanborns].class.name && 0 < prices[:sanborns]
     
     worksheet.save
   end
