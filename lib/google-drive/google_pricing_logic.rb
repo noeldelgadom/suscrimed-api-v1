3ffe9a41c0878ba4cbe560d0bb3eb5474b7ad912
Noel Delgado <noeldelgadom@gmail.com>
2020-11-02 15:34:00 -0600

    pricing logic v1 end of attempt for now

--- lib/google-drive/google_pricing_logic.rb
+++ lib/google-drive/google_pricing_logic.rb
@@ -5,35 +5,26 @@ module GooglePricingLogic
   # reload! ; load "lib/google-drive/google_pricing_logic.rb" ; include GooglePricingLogic ; GooglePricingLogic.version_1
 
   def self.version_1
-    # session                       = GoogleDrive::Session.from_service_account_key("config/api-keys/google-sheets-key.json")
-    # competitor_prices_spreadsheet = session.spreadsheet_by_title("Competitor Prices")
-    # competitor_prices             = competitor_prices_spreadsheet.worksheets.fifth
-    # nadro_spreadsheet             = session.spreadsheet_by_title("NADRO - CATALAGO DE PRODUCTOS NADRO AL 200712")
-    # nadro_costs                   = nadro_spreadsheet.worksheets.first
-    # webflow_spreadsheet           = session.spreadsheet_by_title("Webflow Product Import")
-    # webflow_import                = webflow_spreadsheet.worksheets.first
+    session                       = GoogleDrive::Session.from_service_account_key("config/api-keys/google-sheets-key.json")
+    competitor_prices_spreadsheet = session.spreadsheet_by_title("Competitor Prices")
+    competitor_prices             = competitor_prices_spreadsheet.worksheets.fifth
+    nadro_spreadsheet             = session.spreadsheet_by_title("NADRO - CATALAGO DE PRODUCTOS NADRO AL 200712")
+    nadro_costs                   = nadro_spreadsheet.worksheets.first
+    webflow_spreadsheet           = session.spreadsheet_by_title("Webflow Product Import")
+    webflow_import                = webflow_spreadsheet.worksheets.first
 
     puts'--------------------------------------------------------------------------------'
     puts'--------------------------------------------------------------------------------'
     puts Time.now.to_s + ' Start Updating Google Prices'
 
-    # puts competitor_prices[2,3]
-    # puts nadro_costs[7,3]
-    # puts webflow_import[1,1]
-
 
     puts 'Cost | Max_limit | Limit_Margin | Price | Price_Margin | Farmalisto | Farmalisto_Margin'
-    row       = 1
-    row_limit = 15
-    while row < row_limit
-
-      cost              = rand(1..100) / 1.0
-
-      max_limit         = rand(1..100) / 1.0
-      limit_margin      = (100 * (max_limit - cost) / cost).round(2)
-
-      farmalisto_price  = [rand(1..100) / 1.0, 'Not In Store', '' ].sample
-      farmalisto_margin = farmalisto_price.class.name == 'String' ? 'NA' : (100 * (farmalisto_price - cost) / cost).round(2)
+    row           = 1
+    save_interval = 10
+    while webflow_import[row + 1, 1] != ''
+      cost              = nadro_costs[row + 6, 7]
+      max_limit         = nadro_costs[row + 6, 8]
+      farmalisto_price  = competitor_prices[row + 1, 3]
       
       if farmalisto_price.class.name == 'Float' && cost < farmalisto_price && farmalisto_price < max_limit
         price = farmalisto_price
@@ -45,23 +36,26 @@ module GooglePricingLogic
         end
       end
 
-      price_margin      = (100 * (price - cost) / cost).round(2)
+      webflow_import[row + 1, 7] = price
 
       print cost
       print '    |    '
       print max_limit
       print '    |    '
-      print limit_margin
+      print (100 * (max_limit - cost) / cost).round(2)
       print '    |    '
       print price
       print '    |    '
-      print price_margin
+      print ((price - cost) / cost).round(2)
       print '    |    '
       print farmalisto_price
       print '    |    '
-      print farmalisto_margin
+      print farmalisto_price.class.name == 'String' ? 'NA' : (100 * (farmalisto_price - cost) / cost).round(2)
       puts
-      
+
+
+      webflow_import.save if row % save_interval == 0
+      puts '--- SAVED ---'
       row += 1
     end
 
